afaa7dead1301b5424ed56fae518b3d3b794679d
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-04 15:31:52 +1000

    Used each insted for loop

--- swap_case.rb
+++ swap_case.rb
@@ -2,9 +2,9 @@ class SwapCase
   @@file_name = 'swap_case.txt'
   def swap_case
       File.open(@@file_name).each_line do |line|
-        for i in 0...line.length
+        (0...line.length).each { |i|
           line[i] = line[i] == line[i].upcase ? line[i].downcase : line[i].upcase
-        end
+        }
 
         puts line
       end
